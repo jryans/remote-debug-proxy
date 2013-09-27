4604fb6b69aafaf79ee4cd963d7fdd634edfbe36
J. Ryan Stinnett <jryans@gmail.com>
2013-09-27 17:43:08 -0500

    Send messages to status

--- status/app/scripts/app.js
+++ status/app/scripts/app.js
@@ -2,5 +2,11 @@
 define([], function () {
   'use strict';
 
+  var socket = new WebSocket('ws://localhost:6001');
+
+  socket.onmessage = function (event) {
+    console.log(event.data);
+  };
+
   return '\'Allo \'Allo!';
 });
