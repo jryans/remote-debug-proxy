491b6cdfc8605c4806fa91a0e275a4657ae37333
J. Ryan Stinnett <jryans@gmail.com>
2013-09-29 03:24:00 -0500

    Print log messages on page

--- status/app/scripts/app.js
+++ status/app/scripts/app.js
@@ -2,11 +2,15 @@
 define([], function () {
   'use strict';
 
-  var socket = new WebSocket('ws://localhost:6001');
+  var socket = new WebSocket('ws://localhost:6001/status');
 
   socket.onmessage = function (event) {
-    console.log(event.data);
+    var statusUpdate = JSON.parse(event.data);
+    var message = "";
+    message += "[" + statusUpdate.id + "] ";
+    message += statusUpdate.direction + " ";
+    message += statusUpdate.actor + ":\n";
+    message += statusUpdate.packet + "\n";
+    $('#status').prepend(message);
   };
-
-  return '\'Allo \'Allo!';
 });
