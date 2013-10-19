62d9a8beffefa4a78c74d7bbde2d91a1fd1852de
J. Ryan Stinnett <jryans@gmail.com>
2013-10-19 03:39:45 -0500

    Launching scripts

---
+++ tools/ch-canary-client.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary --no-first-run --user-data-dir=profiles/canary-client
---
+++ tools/ff-aurora-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/FirefoxAurora.app/Contents/MacOS/firefox-bin -no-remote -P server
