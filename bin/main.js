dc917493b6eeff21f7ff3a2e4d1bc79ad3d0d934
J. Ryan Stinnett <jryans@gmail.com>
2013-10-25 02:49:36 -0500

    Add simple options for C/S type

--- bin/main.js
+++ bin/main.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node --harmony
 'use strict';
 
+let opts = require('optimist').argv;
 let cauldron = require('../lib/cauldron');
 
-cauldron.start();
+let specKey;
+if (opts.f) {
+  specKey = 'Firefox 26';
+} else if (opts.c) {
+  specKey = 'Chrome 32';
+}
+
+cauldron.start(specKey);
